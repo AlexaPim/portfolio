8f0a91da393cc7303454d26d001fc219a09e0f03
AlexaPim <alexssandraprado@hotmail.com>
2022-02-10 14:46:15 -0300

    Add page ate portfolio

---
+++ static/script.js
@@ -0,0 +1,22 @@
+const menuMobile = document.querySelector('.menu-mobile')
+const body = document.querySelector('body')
+
+menuMobile.addEventListener('click', () => {
+    menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("menu-nav-active")
+});
+
+/* Fecha o menu quando clicar em algum item e muda o icone para list */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
+})
